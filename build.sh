77339deafe79d862050ad039034d7a28514c58a0
erick-sudo <erickochieng766@gmail.com>
2024-07-15 01:57:34 +0300

    Commit

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run migrations
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Seed Database
-# python3 manage.py loaddata superusers.json
+python3 manage.py loaddata superusers.json
 
 
 # # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
